da3c65424a90ba4b5147588830e660551149a1f5
Tan-DESKPC <tan@tanho.ca>
2021-07-13 08:51:20 -0400

    loose roadmap and goals discussion

---
+++ R/main.R
@@ -0,0 +1,49 @@
+#' Load Play By Play
+#'
+#' @param seasons
+#'
+#' @return
+#' @export
+#'
+#' @examples
+load_pbp <- function(seasons){}
+
+#' Load Player Level Stats
+#'
+#' @param seasons a numeric vector of seasons to return
+#' @param summarise_by one of `week` or `season`
+#' @param type one of `offense`, `defense`, or `special_teams`
+#' @return
+#'
+#' @examples
+#' @export
+load_player_stats <- function(seasons, type, summarise_by){}
+
+#' Load Team Level Stats
+#'
+#' @param seasons a numeric vector of seasons to return
+#' @param summarise_by one of `week` or `season`
+#' @param type one of `offense`, `defense`, or `special_teams`
+#'
+#' @examples
+#' @return
+#' @export
+load_team_stats <- function(){}
+
+#' Load Game Data
+#'
+#' @return
+#' @export
+#'
+#' @examples
+load_schedules <- function(){}
+
+#' Load Rosters
+#'
+#' @param seasons a numeric vector of seasons to return
+#'
+#' @return
+#' @export
+#'
+#' @examples
+load_rosters <- function(season){}
