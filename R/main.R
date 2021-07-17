3988bf44d7b28f2b4da67244b8e48b30ed302816
mrcaseb <roemerle@gmail.com>
2021-07-17 22:11:05 +0200

    add types and progressr

--- R/main.R
+++ R/main.R
@@ -7,19 +7,46 @@
 #'
 #' @examples
 #' load_pbp(2019:2020)
-load_pbp <- function(seasons){
+load_pbp <- function(seasons, type = c("rds", "parquet", "qs")) {
+  type <- match.arg(type)
+
+  # Catch missing packages
+  if (type == "parquet" && !all(is_installed("arrow"), is_installed("curl"))){
+    cli::cli_abort("Packages {.code arrow} and {.code curl} required for argument {.val {type}}. Please install them.")
+  }
+
+  if (type == "qs" && !all(is_installed("qs"), is_installed("curl"))){
+    cli::cli_abort("Packages {.code qs} and {.code curl} required for argument {.val {type}}. Please install them.")
+  }
+
+  # Catch invalid season input
   most_recent <- most_recent_season()
   if (!all(seasons %in% 1999:most_recent)) {
     cli::cli_abort("Please pass valid seasons between 1999 and {most_recent}")
   }
-  urls <- paste0(
+
+  # If progressr is installed, make progress updates available
+  p <- NULL
+  if (is_installed("progressr")) p <- progressr::progressor(along = seasons)
+
+  base_url <- paste0(
     "https://github.com/nflverse/nflfastR-data/blob/master/data/play_by_play_",
-    seasons,
-    ".rds?raw=true"
+    seasons
   )
-  out <- parallel::mclapply(urls, rds_from_url)
-  out <- do.call("rbind", out)
-  out
+
+  # choose loading function and correct urls
+  if (type == "parquet"){
+    loader <- parquet_from_url
+    urls <- paste0(base_url, ".parquet?raw=true")
+  } else if (type == "qs"){
+    loader <- qs_from_url
+    urls <- paste0(base_url, ".qs?raw=true")
+  } else {
+    loader <- rds_from_url
+    urls <- paste0(base_url, ".rds?raw=true")
+  }
+
+  purrr::map_dfr(urls, loader, p = p)
 }
 
 #' Load Player Level Stats
