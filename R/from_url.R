1449c17e33f9feb3a3f797d26126790a6291ffcb
mrcaseb <roemerle@gmail.com>
2021-07-24 23:03:21 +0200

    fix too long example lines and remove unnecessary code

--- R/from_url.R
+++ R/from_url.R
@@ -3,7 +3,6 @@
 #' @param url a character url
 #' @param ... for internal usage only
 #'
-#' @return
 #' @export
 #'
 #' @examples
@@ -27,18 +26,15 @@ rds_from_url <- function(url, ...){
 #' @param url a character url
 #' @param ... for internal usage only
 #'
-#' @return
 #' @export
 #'
 #' @examples
-#' parquet_from_url("https://github.com/nflverse/nflfastR-data/blob/master/data/play_by_play_2020.parquet?raw=true")
+#' raw_from_url(
+#' "https://github.com/nflverse/nflfastR-data/blob/master/data/play_by_play_2020.parquet?raw=true"
+#' )
 raw_from_url <- function(url, ...){
   dots <- list(...)
   if ("p" %in% names(dots)) p <- dots$p else p <- NULL
-
-  if (!all(is_installed("arrow"), is_installed("curl"))){
-    cli::cli_abort("Packages {.code arrow} and {.code curl} required to run this function. Please install them.")
-  }
 
   load <- try(curl::curl_fetch_memory(url), silent = TRUE)
 
@@ -53,17 +49,15 @@ raw_from_url <- function(url, ...){
 #' @param url a character url
 #' @param ... for internal usage only
 #'
-#' @return
 #' @export
 #'
 #' @examples
-#' qs_from_url("https://github.com/nflverse/nflfastR-data/blob/master/data/play_by_play_2020.qs?raw=true")
+#' qs_from_url(
+#' "https://github.com/nflverse/nflfastR-data/blob/master/data/play_by_play_2020.qs?raw=true"
+#' )
 qs_from_url <- function(url, ...){
   dots <- list(...)
   if ("p" %in% names(dots)) p <- dots$p else p <- NULL
-  if (!all(is_installed("qs"), is_installed("curl"))){
-    cli::cli_abort("Packages {.code qs} and {.code curl} required to run this function. Please install them.")
-  }
   load <- try(curl::curl_fetch_memory(url), silent = TRUE)
   if (!is.null(p) && inherits(p, "progressor") && is_installed("progressr")) p("loading ...")
   if (inherits(load, "try-error")) return(data.frame())
