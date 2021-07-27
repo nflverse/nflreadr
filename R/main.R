893a7a4c844e58fa255435f7d7ffcc759a1bdf77
Tan-DESKPC <tan@tanho.ca>
2021-07-26 23:37:54 -0400

    setting default seasons and documenting

--- R/main.R
+++ R/main.R
@@ -2,7 +2,7 @@
 #'
 #' @description Loads multiple seasons from the nflfastR data repository
 #'
-#' @param seasons A numeric vector of 4-digit years associated with given NFL seasons.
+#' @param seasons A numeric vector of 4-digit years associated with given NFL seasons - defaults to latest season
 #' @param file_type One of `"rds"` or `"qs"`. Can also be set globally with
 #' `options(nflreadr.prefer)`
 #'
@@ -18,17 +18,19 @@
 #' @seealso <https://www.nflfastr.com/reference/build_nflfastR_pbp.html> for the nflfastR function `build_nflfastR_pbp()`
 #'
 #' @export
-load_pbp <- function(seasons, file_type = getOption("nflreadr.prefer", default = "qs")) {
+load_pbp <- function(seasons = most_recent_season(), file_type = getOption("nflreadr.prefer", default = "qs")) {
 
   file_type <- match.arg(file_type, c("rds", "qs"))
   loader <- choose_loader(file_type)
 
-  # Catch invalid season input
-  most_recent <- most_recent_season()
+  # Alternative "error" message/handling?
+  stopifnot(is.numeric(seasons),
+            seasons >=1999,
+            seasons <= most_recent_season())
 
-  if (!all(seasons %in% 1999:most_recent)) {
-    stop(paste("Please pass valid seasons between 1999 and", most_recent))
-  }
+  # if (!all(seasons %in% 1999:most_recent_season())) {
+  #   stop(paste("Please pass valid seasons between 1999 and", most_recent_season()))
+  # }
 
   urls <- paste0("https://github.com/nflverse/nflfastR-data/raw/master/data/play_by_play_",
                  seasons, ".", file_type)
@@ -44,7 +46,7 @@ load_pbp <- function(seasons, file_type = getOption("nflreadr.prefer", default =
 
 #' Load Player Level Weekly Stats
 #'
-# @param seasons a numeric vector of seasons to return
+#' @param seasons a numeric vector of seasons to return, if NULL returns all available data
 # @param stat_type one of `offense`, `defense`, or `special_teams`
 #' @param file_type One of `"rds"` or `"qs"`. Can also be set globally with
 #' `options(nflreadr.prefer)`
@@ -60,9 +62,13 @@ load_pbp <- function(seasons, file_type = getOption("nflreadr.prefer", default =
 #' @seealso <https://github.com/nflverse/nflfastr-data>
 #'
 #' @export
-load_player_stats <- function(#seasons,
+load_player_stats <- function(seasons = NULL,
                               # stat_type = c("offense","defense","special_teams"),
                               file_type = getOption("nflreadr.prefer", default = "qs")){
+
+  if(!is.null(seasons)) stopifnot(is.numeric(seasons),
+                                  seasons >=1999,
+                                  seasons <= most_recent_season())
 
   file_type <- match.arg(file_type, c("rds", "qs"))
   loader <- choose_loader(file_type)
@@ -71,6 +77,7 @@ load_player_stats <- function(#seasons,
 
   out <- loader(url)
   class(out) <- c("tbl_df","tbl","data.frame")
+  if(!is.null(seasons)) out <- dplyr::filter(out, out$season %in% seasons)
   out
 }
 
@@ -86,33 +93,39 @@ load_player_stats <- function(#seasons,
 #'
 #' @return
 #'
-#' @export
+# @export
 load_team_stats <- function(){}
 
 #' Load Game/Schedule Data
 #'
 #' This returns game/schedule information as maintained by Lee Sharpe.
 #'
-#' @return A tibble of season-level roster data.
+#' @param seasons a numeric vector of seasons to return, if NULL returns all available data.
+#'
+#' @return A tibble of game information for past and/or future games.
 #'
 #' @seealso <https://github.com/nflverse/nfldata>
-#' @seealso <https://www.nflfastr.com/reference/fast_scraper_roster.html>
 #'
 #' @examples
 #' \donttest{
-#'  load_schedules()
+#'  load_schedules(2020)
 #' }
 #'
 #' @export
-load_schedules <- function(){
+load_schedules <- function(seasons = NULL){
+
   out <- rds_from_url("https://github.com/nflverse/nfldata/raw/master/data/games.rds")
   class(out) <- c("tbl_df","tbl","data.frame")
+
+  if(!is.null(seasons)) stopifnot(is.numeric(seasons))
+  if(!is.null(seasons)) out <- dplyr::filter(out, out$season %in% seasons)
+
   out
 }
 
 #' Load Rosters
 #'
-#' @param seasons a numeric vector of seasons to return
+#' @param seasons a numeric vector of seasons to return, defaults to returning this year's data if NULL.
 #'
 #' @examples
 #' \donttest{
@@ -125,7 +138,10 @@ load_schedules <- function(){
 #' @seealso <https://www.nflfastr.com/reference/fast_scraper_roster.html>
 #'
 #' @export
-load_rosters <- function(seasons){
+load_rosters <- function(seasons = NULL){
+
+  if(is.null(seasons)) seasons <- as.integer(format(Sys.Date(), format = "%Y"))
+
   # different "most-current-season" logic than for pbp, right?
   current_year <- as.integer(format(Sys.Date(), format = "%Y"))
 
@@ -152,6 +168,7 @@ load_rosters <- function(seasons){
 #' starting with the 2016 season. Three different stat types are available and
 #' the current seasons data updates every night.
 #'
+#' @param seasons a numeric vector specifying what seasons to return, defaults to returning all data available
 #' @param stat_type one of `"passing"`, `"receiving"`, or `"rushing"`
 #' @param file_type One of `"rds"` or `"qs"`. Can also be set globally with the
 #' option nflreadr.prefer
@@ -169,10 +186,17 @@ load_rosters <- function(seasons){
 #' @seealso <https://nextgenstats.nfl.com/stats/rushing> for `stat_type = "rushing"`
 #'
 #' @export
-load_nextgen_stats <- function(stat_type = c("passing", "receiving", "rushing"),
+load_nextgen_stats <- function(seasons = NULL,
+                               stat_type = c("passing", "receiving", "rushing"),
                                file_type = getOption("nflreadr.prefer", default = "qs")){
 
+  if(!is.null(seasons)) stopifnot(is.numeric(seasons),
+                                  seasons >= 2016,
+                                  seasons <= most_recent_season())
+
+
   file_type <- match.arg(file_type, c("rds", "qs"))
+  stat_type <- match.arg(stat_type)
   loader <- choose_loader(file_type)
 
   url <- paste0("https://github.com/nflverse/ngs-data/raw/main/data/ngs_",
@@ -182,5 +206,6 @@ load_nextgen_stats <- function(stat_type = c("passing", "receiving", "rushing"),
 
   out <- loader(url)
   class(out) <- c("tbl_df","tbl","data.frame")
+  if(!is.null(seasons)) out <- dplyr::filter(out, out$season %in% seasons)
   out
 }
