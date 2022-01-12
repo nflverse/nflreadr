fb5b66b66827367a7bb1aa8281aaedee35d6d345
John Edwards <edwards1860@gmail.com>
2022-01-12 10:44:36 -0800

    Update depth chart dictionary, closes #78
    * swapped from magrittr pipe to base pipe
    
    magrittr pipe is used without loading magrittr, swapped to base pipe
    
    * updated data dictionary to reflect end of season DCs #78
    
    * slight modification to further clarify weeks of depth charts

--- data-raw/build_data_dictionaries.R
+++ data-raw/build_data_dictionaries.R
@@ -3,7 +3,7 @@ setwd(here::here())
 dictionary_pbp <- read.csv("data-raw/dictionary_pbp.csv")
 usethis::use_data(dictionary_pbp, overwrite = TRUE)
 
-dictionary_player_stats <- read.csv("data-raw/dictionary_playerstats.csv") %>%
+dictionary_player_stats <- read.csv("data-raw/dictionary_playerstats.csv") |>
   dplyr::mutate_all(stringr::str_squish)
 
 usethis::use_data(dictionary_player_stats, overwrite = TRUE)
