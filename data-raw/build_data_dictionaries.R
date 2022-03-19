44fde6b1d8cac1fdfd28a6c4faa91a660fd4188d
Pranav Rajaram <pranavrajaram@gmail.com>
2022-03-19 14:14:19 -0400

    dictionary updates

--- data-raw/build_data_dictionaries.R
+++ data-raw/build_data_dictionaries.R
@@ -19,7 +19,7 @@ usethis::use_data(dictionary_ff_opportunity, overwrite = TRUE)
 dictionary_rosters <- read.csv("data-raw/dictionary_rosters.csv")
 usethis::use_data(dictionary_rosters, overwrite = TRUE)
 
-dictionary_schedules <- read.csv2("data-raw/dictionary_schedules.csv")
+dictionary_schedules <- read.csv("data-raw/dictionary_schedules.csv")
 usethis::use_data(dictionary_schedules, overwrite = TRUE)
 
 dictionary_nextgen_stats <- read.csv2("data-raw/dictionary_nextgenstats.csv")
