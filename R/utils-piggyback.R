8e93dd28ae3e8918e6a3eedb5d144941908b3e15
Tan-DESKPC <tan@tanho.ca>
2022-04-26 11:46:22 -0400

    suggested changes from seb - option download path, nse releases

--- R/utils-piggyback.R
+++ R/utils-piggyback.R
@@ -3,20 +3,39 @@
 #' This function downloads or updates data from the nflverse-data repository releases,
 #' creating subfolders that match the release structure.
 #'
-#' @param folder_path a folder in which subfolders will be created for each release
-#' @param releases one or more of the available releases, or `TRUE` to include all available releases - defaults to `"pbp"`
-#' @param file_type one of `c("rds","parquet", "csv", "qs")` - defaults to file type specified in `options(nflreadr.prefer)`
+#' @param ... releases to download, provided in either unquoted or character format
+#' (i.e. pbp or "pbp" are both fine)
+#' @param folder_path a folder in which subfolders will be created for each release -
+#' defaults to path specified in `options("nflreadr.download_path)` or "." (the current working directory)
+#' @param file_type one of `c("rds","parquet", "csv", "qs")` -
+#' defaults to file type specified in `options(nflreadr.prefer)` or "rds"
 #'
+#' @examples {
+#' \donttest{
+#' try({
+#'   temp_dir <- tempdir()
+#'
+#'   download_nflverse(combine, contracts, folder_path = temp_dir, file_type = "parquet")
+#'
+#'   unlink(temp_dir) # cleanup
+#' })
+#' }
+#' }
 #' @export
-download_local <- function(folder_path = ".",
-                           releases = "pbp",
-                           file_type = getOption("nflreadr.prefer", default = "rds")){
+download_nflverse <- function(...,
+                              folder_path = getOption("nflreadr.download_path", default = "."),
+                              file_type = getOption("nflreadr.prefer", default = "rds")){
 
+  releases <- as.character(rlang::ensyms(...))
   file_name <- NULL
   tag <- NULL
 
   rlang::check_installed(c("piggyback (> 0.1.1)","fs"))
   rlang::arg_match0(file_type, c("rds","csv","parquet","qs"))
+  stopifnot(
+    length(folder_path) == 1,
+    is.character(folder_path)
+  )
 
   all_releases <- piggyback::pb_releases(repo = "nflverse/nflverse-data", verbose = FALSE)
 
@@ -38,6 +57,11 @@ download_local <- function(folder_path = ".",
 
   download_list <- file_list[grepl(paste0("\\.",file_type,"$"),file_name), list(file_name, tag, path = file.path(folder_path,tag))]
 
+  if(any(!releases %in% download_list$tag)){
+    missing <- releases[!releases %in% download_list$tag]
+    cli::cli_warn("Could not find files of type {.val file_type} for the following releases: {.val {missing}}. Skipping.")
+  }
+
   cli::cli_alert_info("Now downloading {.val {nrow(download_list)}} file(s) to {.file {folder_path}}.")
 
   lapply(split(download_list,download_list$tag),
@@ -48,7 +72,7 @@ download_local <- function(folder_path = ".",
              repo = "nflverse/nflverse-data",
              tag = x$tag[[1]],
              overwrite = TRUE
-             )
+           )
          })
 
   cli::cli_alert_success("Downloaded {.val {nrow(download_list)}} file(s) to {.file {folder_path}}.")
