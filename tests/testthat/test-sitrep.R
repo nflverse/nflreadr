453db0fabc177e363630259345f9ece7b24d7912
mrcaseb <roemerle@gmail.com>
2022-04-28 12:32:24 +0200

    maybe better to skip on cran

--- tests/testthat/test-sitrep.R
+++ tests/testthat/test-sitrep.R
@@ -1,5 +1,5 @@
 test_that("sitrep works", {
-  expect_output(.sitrep("nflreadr"), regexp = "System Info|R version |Running under|nflreadr \\(|Dependencies")
+  skip_on_cran()
   expect_output(.sitrep("nflreadr", recursive = FALSE), regexp = "System Info|R version |Running under|nflreadr \\(")
-  expect_output(.sitrep("nflreadr", recursive = FALSE, header = "nflverse "), regexp = "System Info|R version |Running under|nflverse Packages|nflreadr \\(")
+  expect_output(nflverse_sitrep(pkg = "nflreadr"), regexp = "System Info|R version |Running under|nflverse Packages|nflreadr \\(|nflverse Dependencies")
 })
