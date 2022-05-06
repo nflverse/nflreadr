71a3eadf2b2b5d55cb3ed7e572947db37dc72265
Tan Ho <tan@tanho.ca>
2022-05-06 15:37:50 -0400

    reroute to draft picks from release

--- R/load_draft_picks.R
+++ R/load_draft_picks.R
@@ -15,15 +15,14 @@
 #'
 #' @seealso <https://nflreadr.nflverse.com/articles/dictionary_draft_picks.html> for the web data dictionary
 #' @seealso [`dictionary_draft_picks`] for the data dictionary as bundled within the package
-#' @seealso Issues with this data should be filed here: <https://github.com/nflverse/nfldata>
+#' @seealso Issues with this data should be filed here: <https://github.com/nflverse/nflverse-data>
 #'
 #' @export
 load_draft_picks <- function(seasons = TRUE){
-  url <- "https://raw.githubusercontent.com/nflverse/nfldata/master/data/draft_picks.rds"
+  url <- "https://github.com/nflverse/nflverse-data/releases/download/draft_picks/draft_picks.rds"
   out <- rds_from_url(url)
   if(!isTRUE(seasons)) stopifnot(is.numeric(seasons))
   if(!isTRUE(seasons)) out <- out[out$season %in% seasons]
   class(out) <- c("nflverse_data","tbl_df","tbl","data.table","data.frame")
-  attr(out,"nflverse_type") <- "draft picks (via PFR)"
   out
 }
