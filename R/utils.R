de66fa9dc949e65323ad0ec3162fa8525ad731f1
Tan-DESKPC <tan@tanho.ca>
2022-05-21 13:12:56 -0400

    clean up filenames to have a utils prefix

--- R/utils.R
+++ R/utils.R
@@ -1,28 +1,4 @@
-#### UTILS ###
-
-#' Get Latest Season
-#'
-#' A helper function to choose the most recent season available for a given dataset
-#'
-#' @param roster a TRUE/FALSE flag: if TRUE, returns the current year if March 1st or later. if FALSE, returns the current year if September 1st or later. Otherwise returns current year minus 1.
-#'
-#'
-#' @return season (a four digit numeric)
-#' @export
-most_recent_season <- function(roster = FALSE) {
-  today <- Sys.Date()
-  current_year <- as.integer(format(today, format = "%Y"))
-  current_month <- as.integer(format(today, format = "%m"))
-
-  if ((isFALSE(roster) && current_month >= 9) ||
-      (isTRUE(roster) && current_month >= 3)) {
-    return(current_year)
-  }
-
-  return(current_year - 1)
-}
-
-## internal utils ##
+#### Internal UTILS ###
 
 .datatable.aware <- TRUE
 
