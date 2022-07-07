c1876bdd2debe341e0452bcb0926c02606a3c589
Tan Ho <38083823+tanho63@users.noreply.github.com>
2022-07-07 12:39:15 -0400

    Update test-utils_sitrep.R

--- tests/testthat/test-utils_sitrep.R
+++ tests/testthat/test-utils_sitrep.R
@@ -1,5 +1,6 @@
 test_that("sitrep works", {
   skip_on_cran()
   expect_output(.sitrep("nflreadr", recursive = FALSE), regexp = "System Info|R version |Running under|nflreadr \\(")
+  expect_output(.sitrep("xyz", recursive = FALSE), regexp = "System Info|R version |Running under")
   expect_output(nflverse_sitrep(pkg = "nflreadr"), regexp = "System Info|R version |Running under|nflverse Packages|nflreadr \\(|nflverse Dependencies")
 })
