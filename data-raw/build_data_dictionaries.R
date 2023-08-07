487c1a246866b982f2409e5e13b5608ef47d505b
Manny Martinez <emmanuelmartinez.dev@gmail.com>
2023-08-07 08:07:19 -0400

    Added playerstats_def dictionary (#192)
    * Added playerstats_def dictionary
    
    * version and news bullet
    
    * export dictionary and redocument
    
    * add vignette
    
    * avoid cross ref warning
    
    ---------
    
    Co-authored-by: Sebastian Carl <mrcaseb@gmail.com>

--- data-raw/build_data_dictionaries.R
+++ data-raw/build_data_dictionaries.R
@@ -8,6 +8,10 @@ dictionary_player_stats <- read.csv("data-raw/dictionary_playerstats.csv") |>
   dplyr::mutate_all(stringr::str_squish)
 usethis::use_data(dictionary_player_stats, overwrite = TRUE)
 
+dictionary_player_stats_def <- read.csv("data-raw/dictionary_playerstats_def.csv") |>
+  dplyr::mutate_all(stringr::str_squish)
+usethis::use_data(dictionary_player_stats_def, overwrite = TRUE)
+
 dictionary_ff_playerids <- read.csv("data-raw/dictionary_ff_playerids.csv") |>
   dplyr::mutate_all(stringr::str_squish)
 usethis::use_data(dictionary_ff_playerids, overwrite = TRUE)
