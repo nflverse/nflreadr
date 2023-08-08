0440fad8c88c230a6323a98760f6139cc843d818
Tan Ho <tan@tanho.ca>
2023-08-08 11:49:12 -0400

    bugfix

--- R/utils_cran.R
+++ R/utils_cran.R
@@ -10,7 +10,7 @@
   cores <- min(
     getOption("Ncpus", default = 2L),
     as.integer(Sys.getenv("OMP_THREAD_LIMIT",unset = "2")),
-    floor(Sys.getenv("_R_CHECK_EXAMPLE_TIMING_CPU_TO_ELAPSED_THRESHOLD_", unset = 2)),
+    floor(as.integer(Sys.getenv("_R_CHECK_EXAMPLE_TIMING_CPU_TO_ELAPSED_THRESHOLD_", unset = 2))),
     2L,
     na.rm = TRUE
   )
