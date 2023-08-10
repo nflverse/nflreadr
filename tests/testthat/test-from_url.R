2dbe2b05c608278173ea689b7169c659ed840488
Tan Ho <tan@tanho.ca>
2023-08-10 09:04:03 -0400

    wrap qs test in skip if not install

--- tests/testthat/test-from_url.R
+++ tests/testthat/test-from_url.R
@@ -7,19 +7,20 @@ test_that("from_url fails nicely", {
     rds_from_url("https://github.com/nflverse/nfldata/blob/master/data/games.rds"),
     regexp = "Failed to readRDS"
   )
-  expect_warning(
-    qs_from_url("https://github.com/nflverse/nfldata/raw/master/data/games.rds"),
-    regexp = "Failed to parse file"
-  )
-
-  expect_warning(
-    qs_from_url("https://asfdjklasfsadffasd.com"),
-    regexp = "Failed to retrieve data"
-  )
 
   expect_warning(
     raw_from_url("https://github.com/nflverse/nfl/blob/master/data/games.rd"),
     regexp = "HTTP error"
+  )
+
+  skip_if(!rlang::is_installed("qs"))
+  expect_warning(
+    qs_from_url("https://github.com/nflverse/nfldata/raw/master/data/games.rds"),
+    regexp = "Failed to parse file"
+  )
+  expect_warning(
+    qs_from_url("https://asfdjklasfsadffasd.com"),
+    regexp = "Failed to retrieve data"
   )
 
 })
