ddb02703903aabfb861c74f8cf7f0d105cd09010
Sebastian Carl <mrcaseb@gmail.com>
2023-08-18 14:09:07 +0200

    set attributes with data.table

--- R/utils.R
+++ R/utils.R
@@ -22,8 +22,8 @@ rbindlist_with_attrs <- function(dflist){
   nflverse_timestamp <- attr(dflist[[length(dflist)]], "nflverse_timestamp")
   nflverse_type <- attr(dflist[[length(dflist)]], "nflverse_type")
   out <- data.table::rbindlist(dflist, use.names = TRUE, fill = TRUE)
-  attr(out,"nflverse_timestamp") <- nflverse_timestamp
-  attr(out,"nflverse_type") <- nflverse_type
+  data.table::setattr(out, "nflverse_timestamp", nflverse_timestamp)
+  data.table::setattr(out, "nflverse_type",      nflverse_type)
   out
 }
 
