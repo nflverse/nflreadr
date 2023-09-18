280ef6b2440600a792844fbfae556bb661b9fcc4
mrcaseb <mrcaseb@gmail.com>
2023-09-18 22:15:04 +0200

    Compute Date of Labor Day and use it to decide most recent season

--- R/utils_date.R
+++ R/utils_date.R
@@ -2,8 +2,10 @@
 #'
 #' A helper function to choose the most recent season available for a given dataset
 #'
-#' @param roster a TRUE/FALSE flag: if TRUE, returns the current year if March 15th or later. if FALSE, returns the current year if September 1st or later. Otherwise returns current year minus 1.
-#'
+#' @param roster Either `TRUE` or `FALSE`.
+#' If `TRUE`, will return current year after March 15th, otherwise previous year.
+#' If `FALSE`, will return current year on or after Thursday following Labor Day,
+#' i.e. Thursday after the first Monday in September. Otherwise previous year.
 #'
 #' @rdname latest_season
 #' @return most recent season (a four digit numeric)
@@ -14,8 +16,12 @@ most_recent_season <- function(roster = FALSE) {
   current_year <- as.integer(format(today, format = "%Y"))
   current_month <- as.integer(format(today, format = "%m"))
   current_day <- as.integer(format(today, format = "%d"))
+  # First Monday of September
+  labor_day <- compute_labor_day(current_year)
+  # Thursday following Labor Day is TNF season opener
+  season_opener <- labor_day + 3
 
-  if ((isFALSE(roster) && current_month >= 9) ||
+  if ((isFALSE(roster) && today >= season_opener) ||
       (isTRUE(roster) && current_month == 3 && current_day >= 15) ||
       (isTRUE(roster) && current_month > 3 )) {
 
@@ -92,4 +98,24 @@ get_current_week <- function(use_date = FALSE) {
 
     return(current_week)
   }
+}
+
+#' Compute Date of Labor Day
+#'
+#' Computes the date of the Labor Day, i.e. the first Monday in September, in a given year.
+#'
+#' @param year Numeric or Character year. 4 Digits.
+#' @noRd
+#' @return Object of Class `Date`
+#' @examples
+#' # 2023 Labor Day was on Sep 4th
+#' compute_labor_day(2023)
+compute_labor_day <- function(year){
+  stopifnot(length(year) == 1)
+  earliest <- as.Date(paste(year, "09", "01", sep = "-"))
+  latest <- as.Date(paste(year, "09", "08", sep = "-"))
+  range <- seq(earliest, latest, by = "day")
+  numeric_wdays <- as.POSIXlt(range)$wday
+  labor_day <- range[numeric_wdays == 1][1]
+  labor_day
 }
