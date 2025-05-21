34f086643eb6ee26b733069f8d4cad1dbbdbf6c1
mrcaseb <mrcaseb@gmail.com>
2025-05-21 16:33:15 +0200

    test full names and nicks

--- tests/testthat/test-utils_name_cleaning.R
+++ tests/testthat/test-utils_name_cleaning.R
@@ -4,14 +4,14 @@ test_that("cleaning team abbreviations", {
 
   expected_warning <- "Abbreviations not found in `nflreadr::team_abbr_mapping`: PIE"
 
-  x <- c("PIE","LAR","PIT","CRD", "OAK", "CLV", "PHO")
-  expect_warning(new_abbr <- clean_team_abbrs(x),regexp = expected_warning)
+  x <- c("PIE","LAR","PIT","CRD", "OAK", "CLV", "PHO", "Niners", "49ers", "San Diego Chargers", "Oakland Raiders", "St Louis Rams")
+  expect_warning(new_abbr <- clean_team_abbrs(x), regexp = expected_warning)
   expect_warning(old_abbr <- clean_team_abbrs(x, current_location = FALSE), regexp = expected_warning)
   expect_warning(new_abbr_drop <- clean_team_abbrs(x, keep_non_matches = FALSE),regexp = expected_warning)
 
-  expect_equal(new_abbr,      c("PIE", "LA","PIT","ARI","LV","CLE", "ARI"))
-  expect_equal(new_abbr_drop, c(NA, "LA","PIT","ARI","LV","CLE", "ARI"))
-  expect_equal(old_abbr,      c("PIE", "LA","PIT","ARI","OAK","CLE", "ARI"))
+  expect_equal(new_abbr,      c("PIE", "LA","PIT","ARI","LV","CLE", "ARI", "SF", "SF", "LAC", "LV", "LA"))
+  expect_equal(new_abbr_drop, c(NA, "LA","PIT","ARI","LV","CLE", "ARI", "SF", "SF", "LAC", "LV", "LA"))
+  expect_equal(old_abbr,      c("PIE", "LA","PIT","ARI","OAK","CLE", "ARI", "SF", "SF", "SD", "OAK", "STL"))
   options(o)
 })
 
