025cb335ccc730f52069aa1f66fe1580c6909b90
John Edwards <edwards1860@gmail.com>
2025-08-15 18:31:55 -0700

    swap to `roster = TRUE`

--- R/load_depth_charts.R
+++ R/load_depth_charts.R
@@ -2,7 +2,7 @@
 #'
 #' @description Loads depth charts for each NFL team for each week back to 2001.
 #'
-#' @param seasons a numeric vector specifying what seasons to return, if `TRUE` returns all available data. Defaults to latest season.
+#' @param seasons a numeric vector specifying what seasons to return, if `TRUE` returns all available data. Defaults to latest season with available rosters.
 #' @param file_type One of `c("rds", "qs", "csv", "parquet")`. Can also be set globally with
 #' `options(nflreadr.prefer)`
 #'
@@ -20,14 +20,22 @@
 #'
 #' @return A tibble of week-level depth charts for each team.
 #' @export
-load_depth_charts <- function(seasons = most_recent_season(),
-                              file_type = getOption("nflreadr.prefer", default = "rds")){
-  if(isTRUE(seasons)) seasons <- 2001:most_recent_season()
-  stopifnot(is.numeric(seasons),
-            seasons >= 2001,
-            seasons <= most_recent_season())
+load_depth_charts <- function(
+  seasons = most_recent_season(roster = TRUE),
+  file_type = getOption("nflreadr.prefer", default = "rds")
+) {
+  if (isTRUE(seasons)) {
+    seasons <- 2001:most_recent_season(roster = TRUE)
+  }
+  stopifnot(
+    is.numeric(seasons),
+    seasons >= 2001,
+    seasons <= most_recent_season(roster = TRUE)
+  )
 
-  urls <- glue::glue("https://github.com/nflverse/nflverse-data/releases/download/depth_charts/depth_charts_{seasons}.rds")
+  urls <- glue::glue(
+    "https://github.com/nflverse/nflverse-data/releases/download/depth_charts/depth_charts_{seasons}.rds"
+  )
   out <- load_from_url(urls, seasons = seasons, nflverse = TRUE)
   return(out)
 }
