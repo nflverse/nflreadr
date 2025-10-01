729bffc8b9145ebbd281fcc42b5470acf8bd546f
mrcaseb <mrcaseb@gmail.com>
2025-10-01 09:47:18 +0200

    accepts file_type arg and change base url to nflverse-data release

--- R/load_teams.R
+++ R/load_teams.R
@@ -4,6 +4,9 @@
 #'
 #' @param current If `TRUE` (the default), returns a standardized list of current teams only,
 #' with abbreviations as per [nflreadr::team_abbr_mapping].
+#' @param file_type One of `c("rds", "qs", "csv", "parquet")`. Can also be set globally with
+#' `options(nflreadr.prefer)`
+#'
 #' @examples
 #' \dontshow{.for_cran()}
 #' \donttest{
@@ -17,12 +20,19 @@
 #' @seealso Issues with this data should be filed here: <https://github.com/nflverse/nflverse-pbp>
 #'
 #' @export
-load_teams <- function(current = TRUE) {
-  out <- load_from_url(
-    "https://github.com/nflverse/nflverse-pbp/raw/master/teams_colors_logos.rds",
-    nflverse = TRUE,
-    nflverse_type = "team graphics"
+load_teams <- function(
+  current = TRUE,
+  file_type = getOption("nflreadr.prefer", default = "rds")
+) {
+  file_type <- rlang::arg_match0(file_type, c("rds", "qs", "csv", "parquet"))
+
+  urls <- paste0(
+    "https://github.com/nflverse/nflverse-data/releases/download/teams/",
+    "teams_colors_logos.",
+    file_type
   )
+
+  out <- load_from_url(urls, nflverse = TRUE)
 
   if (isTRUE(current)) {
     out <- out[out$team_abbr %in% nflreadr::team_abbr_mapping, ]
